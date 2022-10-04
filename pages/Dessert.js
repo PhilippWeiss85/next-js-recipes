6f01fce89ed009d0eb7284a63d02d6040280d23f
Philipp Weiss <p.weiss85@gmx.de>
2022-10-04 17:06:44 +0200

    add pie picture to dessert.js

--- pages/Dessert.js
+++ pages/Dessert.js
@@ -1,6 +1,15 @@
+import Image from "next/image";
+
 export default function Dessert() {
   return (
     <div>
+      <Image
+        src="https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400"
+        alt="pie"
+        layout="responsive"
+        width={1599}
+        height={2400}
+      />
       <h1>Hello Dessert</h1>
       <p>This is a text</p>
     </div>
