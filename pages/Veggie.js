76c2cb3627a38ae6e08da4670fe291f0d9eae1c6
Philipp Weiss <p.weiss85@gmx.de>
2022-10-04 17:00:04 +0200

    add pizza image

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,6 +1,15 @@
+import Image from "next/image";
+
 export default function Veggie() {
   return (
     <div>
+      <Image
+        src="/assets/pizza.jpg"
+        alt="pizza"
+        layout="responsive"
+        width={2400}
+        height={1597}
+      />
       <h1>Hello Veggie</h1>
       <p>This is a text</p>
     </div>
